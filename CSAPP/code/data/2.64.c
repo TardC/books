9703bf5d57cb5dc23a5444f3857cd9a5de1b87fb
TardC <tardc2016@gmail.com>
2018-05-12 12:01:56 +0800

    modify 2.64.c

--- CSAPP/code/data/2.64.c
+++ CSAPP/code/data/2.64.c
@@ -5,6 +5,14 @@
 int any_odd_one(unsigned x)
 {
     unsigned mask = 0xAAAAAAAA;
+    return !!(mask & x);
+}
+
+/* Return 1 when all odd bit of x equals 1; 0 otherwise.
+   Assume w=32 */
+int all_odd_one(unsigned x)
+{
+    unsigned mask = 0xAAAAAAAA;
     return !((mask & x) ^ mask);
 }
 
@@ -14,4 +22,6 @@ int main()
     unsigned test_num2 = 0x00000000;
 
     printf("%d %d\n", any_odd_one(test_num1), any_odd_one(test_num2));
+    printf("%d %d\n", all_odd_one(test_num1), all_odd_one(test_num2));
+    return 0;
 }
