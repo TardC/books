57ed32f6ac768b36e42ddaf4c996e931034bc2c6
TardC <tardc2016@gmail.com>
2018-05-13 11:31:40 +0800

    modify 2.65.c

--- CSAPP/code/data/2.65.c
+++ CSAPP/code/data/2.65.c
@@ -14,9 +14,26 @@ int odd_ones(unsigned x)
     return x;
 }
 
+int odd_ones2(unsigned x)
+{
+    /* $begin count bits of 1 */
+    x = (x & 0x55555555) + ((x >> 1) & 0x55555555);
+    x = (x & 0x33333333) + ((x >> 2) & 0x33333333);
+    x = (x & 0x0f0f0f0f) + ((x >> 4) & 0x0f0f0f0f);
+    x = (x & 0x00ff00ff) + ((x >> 8) & 0x00ff00ff);
+    x = (x & 0x0000ffff) + ((x >> 16) & 0x0000ffff);
+    /* $end count bits of 1 */
+
+    /* if number of 1 is odd, the lsb is 1, otherwise 0 */
+    x &= 0x1;
+    return x;
+}
+
 int main()
 {
     printf("%d\n", odd_ones(0x10101011));
     printf("%d\n", odd_ones(0x01010101));
+    printf("%d\n", odd_ones2(0x10101011));
+    printf("%d\n", odd_ones2(0x01010101));
     return 0;
 }
